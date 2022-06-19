fa532947b4e20c740fb9fa6aea1f28ce0efd691b
mcsdevbr <m.c.s@msn.com>
2022-06-19 20:33:02 -0300

    js

---
+++ Manipulating DOM/js/aula06a.js
@@ -0,0 +1,27 @@
+let caixaElement = document.querySelector('.caixa');
+
+caixaElement.style.width = '177px';
+caixaElement.style.height = '177px';
+caixaElement.style.border = '5px solid red';
+caixaElement.style.backgroundColor = 'black';
+
+/*
+ As configurações de estilos via JavaScript seguem 
+ os mesmos conceitos e atributos utilizados via CSS.
+
+ OBS: Diferença
+
+ Em CSS onde usa-se background-color
+ Em JavaScript usa-se backgroundColor
+
+ Outro exemplo:
+
+ Em CSS onde usa-se font-size
+ Em JavaScript usa-se fontSize
+
+ Mais um exemplo:
+
+ Em CSS onde usa-se margin-bottom
+ Em JavaScript usa-se marginBottom
+ 
+*/
