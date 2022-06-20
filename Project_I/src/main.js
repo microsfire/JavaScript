e2f80b27be4cd72d10ed28b90e1bd05e05cb7ab8
mcsdevbr <m.c.s@msn.com>
2022-06-20 19:35:21 -0300

    Weppack

---
+++ Project_I/src/main.js
@@ -0,0 +1,22 @@
+/*
+//Dando apelido para a funçao soma
+import {soma as somarMais, subtracao} from './helper';
+//Default não precisa de chaves
+import dobrar from './dobrar';
+import quadrado, {metade, MEUPI} from './funcoes';
+//Importando todas as funcoes comum do helper
+import * as helpers from './helper';
+
+
+// console.log(soma(5, 5));
+console.log(subtracao(5, 3));
+console.log(dobrar(7));
+console.log(somarMais(7, 14));
+console.log(metade(5));
+console.log(MEUPI);
+console.log(quadrado(10));
+console.log(helpers.soma(10, 89));
+console.log(helpers.subtracao(10, 9));
+*/
+
+document.querySelector('body').style.background = 'yellow';
