af7c25a8c7c47e27c035b0ae908cb72d6804bffd
mcsdevbr <m.c.s@msn.com>
2022-07-06 21:11:39 -0300

    useState e useEffect

--- React/react-hooks/react-http/src/components/Usuarios/Usuarios.js
+++ React/react-hooks/react-http/src/components/Usuarios/Usuarios.js
@@ -1,77 +1,61 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import AdicionarUsuario from '../AdicionarUsuario/AdicionarUsuario'
 import Usuario from '../Usuario/Usuario'
 
-class Usuarios extends Component {
+function Usuarios (props) {
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      usuarios: [
-        { id: 1, nome: 'João', sobrenome: 'Silva', email: 'joao@mail.com' },
-        { id: 2, nome: 'Maria', sobrenome: 'Santos', email: 'maria@mail.com' }
-      ]
-    }
+  const [usuarios, setUsuarios] = useState([])
 
-    this.adicionarUsuario = this.adicionarUsuario.bind(this)
+  useEffect(() => {
+    //METODO GET o fetch é por padrão
+   fetch('https://reqres.in/api/users')
+   .then(resposta => resposta.json())
+   .then(dados => {
+     //console.log(dados.data)
+
+     const usuarios = dados.data.map(usuario => ({
+       id: usuario.id,
+       nome: usuario.first_name,
+       sobrenome: usuario.last_name,
+       email: usuario.email
+     }))
+     setUsuarios(usuarios)
+   })
+
+  }, [])
+  
+
+  const adicionarUsuario = usuario => {
+    setUsuarios(usuariosAtuais => [...usuariosAtuais, usuario])
   }
 
-  adicionarUsuario(usuario) {
-    const usuarios = [...this.state.usuarios, usuario]
-    this.setState({ usuarios: usuarios })
-  }
-
-  removerUsuario(usuario) {
+  const removerUsuario = usuario => {
     if (window.confirm(`Tem certeza que deseja remover "${usuario.nome} ${usuario.sobrenome}"?`)) {
       //METODO DELETE
       fetch(`https://reqres.in/api/users/${usuario.id}`,{
         method: 'DELETE'
       })
         .then(resposta =>{
-          console.log(resposta)
           if(resposta.ok){
-            let usuarios = this.state.usuarios
-            usuarios = usuarios.filter(x => x.id !== usuario.id)
-            this.setState({ usuarios: usuarios })
+            setUsuarios(usuarios.filter(x => x.id !== usuario.id))
           }   
         })
     }
   }
 
-  componentDidMount(){
-    //METODO GET o fetch é por padrão
-   fetch('https://reqres.in/api/users')
-    .then(resposta => resposta.json())
-    .then(dados => {
-      //console.log(dados.data)
+  return (
+    <>
+      <AdicionarUsuario adicionarUsuario={adicionarUsuario} />
 
-      const usuarios = dados.data.map(usuario => ({
-        id: usuario.id,
-        nome: usuario.first_name,
-        sobrenome: usuario.last_name,
-        email: usuario.email
-      }))
-      //console.log(usuarios,)
-      //this.setState({usuarios: usuarios})
-      this.setState({usuarios})
-    })
-  }
-
-  render() {
-    return (
-      <>
-        <AdicionarUsuario adicionarUsuario={this.adicionarUsuario} />
-
-        {this.state.usuarios.map(usuario => (
-          <Usuario key={usuario.id}
-            usuario={usuario}
-            removerUsuario={this.removerUsuario.bind(this, usuario)}
-          />
-        ))}
-      </>
-    )
-  }
+      {usuarios.map(usuario => (
+        <Usuario key={usuario.id}
+          usuario={usuario}
+          removerUsuario={() => removerUsuario(usuario)}
+        />
+      ))}
+    </>
+  )
 }
 
 export default Usuarios
